e685602c11c975d81b60b4805de0fc19b9f3d84d
QawimHAKIM <qawim.hakim@tekschool.us>
2025-03-25 14:53:41 -0400

    add home view, update repository interfaces, and add home controller

--- src/main/java/com/dev/projectmanagement/repository/EmployeeRepository.java
+++ src/main/java/com/dev/projectmanagement/repository/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.dev.projectmanagement.repository;
 import com.dev.projectmanagement.entity.Employee;
 import org.springframework.data.repository.CrudRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+import java.util.List;
 
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    @Override
+    public List<Employee> findAll();
 }
--- src/main/java/com/dev/projectmanagement/repository/ProjectRepository.java
+++ src/main/java/com/dev/projectmanagement/repository/ProjectRepository.java
@@ -3,6 +3,9 @@ package com.dev.projectmanagement.repository;
 import com.dev.projectmanagement.entity.Project;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProjectRepository extends CrudRepository<Project, Long> {
+import java.util.List;
 
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    public List<Project> findAll();
 }
